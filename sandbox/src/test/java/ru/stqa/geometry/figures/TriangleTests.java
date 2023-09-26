d806d8ebb3b9af1917a6633e0640042cb1bae95b
kenj <kirillbetlij5@gmail.com>
2023-09-26 10:50:04 +0300

    Добавление всех тестов для треугольника

--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -13,4 +13,37 @@ public class TriangleTests {
         } catch (IllegalArgumentException exception) {
         }
     }
+    @Test
+    void cannotCreateRectangleWithNegativeSide(){
+        try{
+            new Rectangle(-5., 3);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception){
+
+        }
+    }
+    @Test
+    void testEquality(){
+        var t1 = new Triangle(5.0,4.0,3.0);
+        var t2 = new Triangle(5.0,4.0, 3.0);
+        Assertions.assertEquals(t1,t2);
+    }
+    @Test
+    void testEquality2(){
+        var t1 = new Triangle(5.0,4.0,4.0);
+        var t2 = new Triangle(5.0,3.0, 3.0);
+        Assertions.assertEquals(t1,t2);
+    }
+    @Test
+    void testNonEquality(){
+        var t1 = new Triangle(5.0,4.0,3.0);
+        var t2 = new Triangle(5.0,5.0, 3.0);
+        Assertions.assertNotEquals(t1,t2);
+    }
+    @Test
+    void testPassed(){
+        var t1 = new Triangle(3.0,5.0,4.0);
+        var t2 = new Triangle(5.0,4.0, 3.0);
+        Assertions.assertTrue(t1.equals(t2));
+    }
 }
