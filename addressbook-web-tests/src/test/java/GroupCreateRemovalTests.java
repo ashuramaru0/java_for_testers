823ed032c3a225e4b9a12026daf0dab94961cfcc
kenj <kirillbetlij5@gmail.com>
2023-09-27 15:21:34 +0300

    Удаление дублирующего модуля

--- addressbook-web-tests/src/test/java/GroupCreateRemovalTests.java
+++ addressbook-web-tests/src/test/java/GroupCreateRemovalTests.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GroupCreationTests {
+public class GroupCreateRemovalTests {
   private WebDriver driver;
   @BeforeEach
   public void setUp() {
@@ -39,6 +39,27 @@ public class GroupCreationTests {
     driver.findElement(By.name("group_footer")).click();
     driver.findElement(By.name("group_footer")).sendKeys("group footer");
     driver.findElement(By.name("submit")).click();
+    driver.findElement(By.linkText("group page")).click();
+  }
+  @Test
+  public void GroupRemovalTest() {
+    if (!isElementPresent(By.name("new"))) {
+      driver.findElement(By.linkText("groups")).click();
+    }
+    if (!isElementPresent(By.name("selected[]"))){
+      driver.findElement(By.linkText("groups")).click();
+      driver.findElement(By.name("new")).click();
+      driver.findElement(By.name("group_name")).click();
+      driver.findElement(By.name("group_name")).sendKeys("group name");
+      driver.findElement(By.name("group_header")).click();
+      driver.findElement(By.name("group_header")).sendKeys("group header");
+      driver.findElement(By.name("group_footer")).click();
+      driver.findElement(By.name("group_footer")).sendKeys("group footer");
+      driver.findElement(By.name("submit")).click();
+      driver.findElement(By.linkText("group page")).click();
+    }
+    driver.findElement(By.name("selected[]")).click();
+    driver.findElement(By.name("delete")).click();
     driver.findElement(By.linkText("group page")).click();
   }
   private boolean isElementPresent(By locator) {
