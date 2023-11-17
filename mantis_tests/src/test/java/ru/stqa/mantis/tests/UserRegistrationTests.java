aedd23f3fb8cee79a06ec85de180fc41ea194348
kenj <kirillbetlij5@gmail.com>
2023-11-17 16:55:46 +0300

    Лекция 9.4

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
@@ -1,26 +1,26 @@
 package ru.stqa.mantis.tests;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.stqa.mantis.common.CommonFunctions;
 import ru.stqa.mantis.model.DeveloperMailUser;
-
 import java.time.Duration;
 import java.util.regex.Pattern;
-
 public class UserRegistrationTests extends TestBase{
-
     DeveloperMailUser user;
     @Test
     void canRegisterUser(){
         var password = "password";
-        user =  app.developerMail().addUser();
+        user = app.developerMail().addUser();
         var email = String.format("%s@developermail.com", user.name());
-    }
 
-    @AfterEach
-    void deleteMailUser () {
-        app.developerMail().deleteUser(user);
+        app.session().signup(user.name(), email);
+
+        var message = app.developerMail().receive(user, Duration.ofSeconds(10));
+        var url = CommonFunctions.extractUrl(message);
+
+        app.session().finishedRegistration(url, user.name(), password);
+
+        app.http().login(user.name(), password);
+        Assertions.assertTrue(app.http().isLoggedIn());
     }
-}
