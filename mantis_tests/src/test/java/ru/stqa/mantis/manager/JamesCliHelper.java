e84f1e0f0ea93f172ffbbd4f79b95c829e30434c
kenj <kirillbetlij5@gmail.com>
2023-11-17 13:27:23 +0300

    Задание №18

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/JamesCliHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/JamesCliHelper.java
@@ -1,22 +1,22 @@
 package ru.stqa.mantis.manager;
-
 import org.openqa.selenium.io.CircularOutputStream;
 import org.openqa.selenium.os.CommandLine;
-
 public class JamesCliHelper extends HelperBase{
-    public JamesCliHelper(ApplicationManager manager){
+    public JamesCliHelper(ApplicationManager manager) {
         super(manager);
     }
-
-    public void addUser(String email, String password){
-        CommandLine cmd = new CommandLine(
-                "java",
+    public void addUser(String email, String password) {
+        CommandLine cmd = new CommandLine("java",
                 "-cp",
                 "\"james-server-jpa-app.lib/*\"",
                 "org.apache.james.cli.ServerCmd",
-                "AddUser",email, password);
+                "--username",
+                "james-admin",
+                "--password",
+                "ViCC4PI0uO",
+                "AddUser", email, password);
         cmd.setWorkingDirectory(manager.property("james.workingDir"));
-        CircularOutputStream out = new CircularOutputStream();
+        CircularOutputStream  out = new CircularOutputStream();
         cmd.copyOutputTo(out);
         cmd.execute();
         cmd.waitFor();
--- mantis_tests/src/test/java/ru/stqa/mantis/manager/SessionHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/SessionHelper.java
@@ -1,21 +1,38 @@
 package ru.stqa.mantis.manager;
-
-import org.openqa.selenium.By;
-
+import org.openqa.selenium.*;
 public class SessionHelper extends HelperBase{
-
-    public SessionHelper(ApplicationManager manager){
+    public SessionHelper (ApplicationManager manager) {
         super(manager);
     }
-
     public void login(String user, String password) {
-        type(By.name("username"),user);
+        type(By.name("username"), user);
         click(By.cssSelector("input[type='submit']"));
-        type(By.name("password"),password);
+        type(By.name("password"), password);
         click(By.cssSelector("input[type='submit']"));
     }
-
     public boolean isLoggedIn() {
         return isElementPresent(By.cssSelector("span.user-info"));
+    }
+    public void signup(String username, String email) {
+        click(By.xpath("//*[@id='login-box']/div/div[2]/a"));
+        type(By.name("username"), username);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[type='submit']"));
+        click(By.xpath("//*[@id='login-box']/div/div/div[4]/a"));
+    }
+
+    public void finishedRegistration(String pattern, String username, String password) {
+        manager.driver().get(pattern);
+        type(By.name("realname"), username);
+        type(By.name("password"), password);
+        type(By.name("password_confirm"), password);
+        click(By.cssSelector("button[type='submit']"));
+
+        type(By.name("username"), username);
+        click(By.cssSelector("input[type='submit']"));
+
+        type(By.name("password"), password);
+        click(By.cssSelector("input[type='submit']"));
+
     }
 }
